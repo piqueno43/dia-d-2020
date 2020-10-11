ac183fe19736ddad38461a7818f468c80ed5625c
Edivaldo Silva <piqueno43@gmail.com>
2020-10-11 08:09:23 -0300

    Criação do menu de acessibilidade e topo

--- src/includes/footer.inc.php
+++ src/includes/footer.inc.php
@@ -1,17 +1,8 @@
     </main>
     <?php $je_footer && include __DIR__ .'/je/footer.inc.php'; ?>
     <!-- end wrapper -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/sharer.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/TweenMax.min.js"></script>
-    <script src="js/clipboard.min.js"></script>
-    <script src="js/menuspy.min.js"></script>
-    <script src="js/menu.js"></script>
-    <script src="js/share.js"></script>
-    <script src="js/collapse.js"></script>
   </body>
 </html>
