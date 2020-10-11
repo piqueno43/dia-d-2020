0b54f83653f8a14cf0c180d2d1a7282bb55d12ff
Edivaldo Silva <piqueno43@gmail.com>
2020-10-11 07:51:22 -0300

    Instalação das fontes

--- src/includes/header.inc.php
+++ src/includes/header.inc.php
@@ -3,32 +3,25 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Fato ou boato</title>
+  <title>Tribunal Superior Eleitoral</title>
   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
   <link id="tag-meta-plone">
   <link id="tag-link-plone">
   <link id="tag-style-plone">
   <link id="claimreview">
 
-  <link rel="stylesheet" href="css/font-awesome.css">
-  <link rel="stylesheet" href="css/fancybox.css"/>
   <link rel="stylesheet" href="css/global.css"/>
   <link rel="stylesheet" href="css/je.css"/>
   <link rel="stylesheet" href="css/styles.css"/>
 
   <meta property="og:url"         content="https://www.justicaeleitoral.jus.br/index.html" />
   <meta property="og:type"        content="website" />
-  <meta property="og:title"       content="Fato ou Boato" />
-  <meta property="og:description" content="Esclarecimentos sobre informações falsas" />
-  <meta property="og:image"       content="https://www.your-domain.com/path/image.jpg" />
+  <meta property="og:title"       content="Tribunal Superior Eleitoral" />
+  <meta property="og:description" content="O portal do TSE abriga informações sobre eleições, serviços ao eleitor, pesquisa de jurisprudência, publicações e outros assuntos de escopo eleitoral." />
+  <meta property="og:image"       content="images/" />
 </head>
 <body>
   <?php $je_topo && include __DIR__ . '/je/header.inc.php' ?>
   <!-- start wrapper -->
   <main class="wrapper">
-    <header class="header">
-      <div class="header-body">
-        <h4 class="header-title"><img src="imagens/logo-fato.png" height="92" alt="Logo fato ou boato"> <img src="imagens/logoEuvotoSemFake.png" alt="Logo eu voto sem fake" height="69"></h4>
-        <p class="header-text">Esclarecimentos sobre informações falsas</p>
-      </div>
-    </header>
+
--- src/index.php
+++ src/index.php
@@ -2,15 +2,5 @@
 $je_topo = true;
 $je_footer = true;
 include __DIR__ .'/includes/header.inc.php' ;
-include __DIR__ .'/includes/spotlights.inc.php' ;
-include __DIR__ .'/includes/menu.inc.php' ;
-include __DIR__ .'/includes/outras-checagens.inc.php' ;
-include __DIR__ .'/includes/o-que-e.inc.php' ;
-include __DIR__ .'/includes/agencias.inc.php' ;
-include __DIR__ .'/includes/urnas.inc.php' ;
-include __DIR__ .'/includes/voto-sem-fake.inc.php' ;
-include __DIR__ .'/includes/minuto-checagem.inc.php' ;
-include __DIR__ .'/includes/como-identificar.inc.php' ;
-include __DIR__ .'/includes/modal.inc.php';
 include __DIR__ .'/includes/footer.inc.php';
 ?>
