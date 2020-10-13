99a7dd74026c567f8f03f56117e7009d3181885b
Edivaldo Silva <piqueno43@gmail.com>
2020-10-13 16:50:55 -0300

    Versão do site

--- src/includes/content.inc.php
+++ src/includes/content.inc.php
@@ -74,10 +74,11 @@
         </div>
       <?php endforeach;?>
   </section>
-
+  <div class="version">
+    <a href="#" class="btn btn-link btn-block">Versão completa do site</a>
+  </div>
   <footer class="footer-whatsapp">
     <h5 class="footer-whatsapp-text">Tira dúvidas eleitoral</h5>
     <button class="btn btn-whatsapp btn-block" type="button">WhatsApp (bot)</button>
   </footer>
-
 </div>
